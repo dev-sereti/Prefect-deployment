f492cee0e757211cad42890509b537ac587c7d6c
dev-sereti <devsereti@gmai.com>
2026-05-14 08:03:52 +0300

    GitHub repo containing your flow code

--- deploy.py
+++ deploy.py
@@ -8,19 +8,19 @@ from pipeline import teen_mental_health_pipeline
 
 if __name__ == "__main__":
 
-    teen_mental_health_pipeline.deploy(
+    source = GitRepository(
+        url="https://github.com/dev-sereti/Prefect-deployment.git",
+        branch="main",
+    )
+
+    teen_mental_health_pipeline.from_source(
+        source=source,
+        entrypoint="pipeline.py:teen_mental_health_pipeline",
+    ).deploy(
         name="teen-mental-health-etl",
 
-        # Prefect managed pool
         work_pool_name="default-work-pool",
 
-        # GitHub repo containing your flow code
-        storage=GitRepository(
-            url="https://github.com/dev-sereti/Prefect-deployment.git",
-            branch="main",
-        ),
-
-        # Run every 60 minutes
         schedules=[
             IntervalSchedule(
                 interval=timedelta(minutes=60)
@@ -36,4 +36,3 @@ if __name__ == "__main__":
     )
 
     print("\nDeployment registered on Prefect Cloud!")
-    print("View runs at: https://app.prefect.io")
